564846b893972515d6ffe5bbe22feb00c4af6b83
JM <64283767+jmperalta89@users.noreply.github.com>
2021-08-03 20:32:58 -0300

    q

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app.layout = html.P("Hola mundo!")
 
 if __name__ == "__main__":
   app.run_server(mode ="external")
+  
+!pip freeze > requirements.txt
+import types
+
+def imports(): 
+   for name, val in globals().items():
+     if isinstance(val, types.ModuleType):
+       if val.__name__ !='builtins':
+         if val.__name__!='types':
+           yield val.__name__
+list(imports())
