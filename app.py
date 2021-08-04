da0c270a2b7d7a5222dd7b7b6fdb6f449d62a4ba
JM <64283767+jmperalta89@users.noreply.github.com>
2021-08-04 00:03:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
+import os
 
-import pandas as pd
-import numpy as np
-
-# import dash
-from jupyter_dash import JupyterDash
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input,Output,State
-import dash_table
 
-import plotly.express as px
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = JupyterDash()
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.P("Hola mundo!")
-
-app = dash.Dash(__name__)
 server = app.server
+
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
