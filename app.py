b42300f7e6b974ad0095a4feb21f3bdce3179ab5
JM <64283767+jmperalta89@users.noreply.github.com>
2021-08-12 20:57:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,3 @@ indices = [['uno','dos','tres']]
 values =  [['1','2','3']]
 nueva_tabla = pd.DataFrame( { 'NUMEROS':indices, 'VALORES': values})
 nueva_tabla
-
-!pip freeze > requirements.txt
-import types
-
-def imports():
-  for name, val in globals().items():
-    if isinstance(val, types.ModuleType):
-      if val.__name__ !='builtins':
-        if val.__name__!='types':
-          yield val.__name__
-list(imports())
